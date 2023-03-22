4339690b723d3971a46243c7ba5843d7c0f3dea1
ar871 <arehman871@hotmail.com>
2023-03-22 20:40:47 +0000

    added bmi calculator

---
+++ src/components/BmiCalculator.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function BmiCalculator() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState("");
+
+  const calculateBmi = () => {
+    const bmiValue = (weight / Math.pow(height / 100, 2)).toFixed(2);
+    setBmi(bmiValue);
+  };
+
+  return (
+    <div>
+      <h1>BMI Calculator</h1>
+      <div>
+        <label>Weight (in kg): </label>
+        <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+      </div>
+      <div>
+        <label>Height (in cm): </label>
+        <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} />
+      </div>
+      <button onClick={calculateBmi}>Calculate BMI</button>
+      <div>
+        <label>BMI: </label>
+        <span>{bmi}</span>
+        <p> If your BMI is 18.5 to 24.9, it falls within the Healthy Weight range. If your BMI is 25.0 to 29.9, it falls within the overweight range. If your BMI is 30.0 or higher, it falls within the obese range.</p>
+      </div>
+    </div>
+  );
+}
+
+export default BmiCalculator;
--- src/components/bodyparts.js
+++ src/components/bodyparts.js
@@ -9,7 +9,7 @@ function Bodyparts() {
       <div>
         <p>hello world</p>
         
-        
+      
         <MuscleGroupImage muscleGroups={["chest","biceps"]}/>
       </div>
     );
