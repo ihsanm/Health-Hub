96cbd235ede527c7b9ba6a3887e0dad286a68ef6
Milena <milenagg77@hotmail.com>
2023-03-24 17:02:46 +0000

    added styles to nutrition

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Bodyparts from './components/bodyparts';
 import BmiCalculator from "./components/BmiCalculator";
-import Nutrition from './components/Nutrition';
+import Nutrition from './components/Nutrition/Nutrition';
 
 function App() {
   return (
